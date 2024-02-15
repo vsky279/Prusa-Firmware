24fab9457edab4273ed608fe0c233b08017b2637
Lukáš Voborský <vsky279@users.noreply.github.com>
2024-02-15 22:50:05 +0100

    Redirect serial communication to UART2

--- Firmware/Configuration.h
+++ Firmware/Configuration.h
@@ -75,10 +75,11 @@ extern const char _sPrinterMmuName[] PROGMEM;
 // SERIAL_PORT selects which serial port should be used for communication with the host.
 // This allows the connection of wireless adapters (for instance) to non-default port pins.
 // Serial port 0 is still used by the Arduino bootloader regardless of this setting.
-#define SERIAL_PORT 0
+#define SERIAL_PORT 2
 
 // This determines the communication speed of the printer
-#define BAUDRATE 115200
+// #define BAUDRATE 115200
+#define BAUDRATE 250000
 
 // Enable g-code compression (see https://github.com/scottmudge/OctoPrint-MeatPack)
 #define ENABLE_MEATPACK
